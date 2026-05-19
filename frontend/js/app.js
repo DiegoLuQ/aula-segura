915d18c827d1507a07a0d2af9071a393ec574536
DiegoLuQ <jdluquelinares@email.com>
2026-05-19 12:51:28 -0400

    mejora los puertos

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,6 +1,6 @@
 const API_URL = window.location.origin.includes('127.0.0.1') || window.location.origin.includes('localhost') || window.location.origin.startsWith('file://')
-    ? 'http://127.0.0.1:8010'
-    : window.location.origin;
+    ? 'http://127.0.0.1:8000'
+    : window.location.origin + '/api';
 let currentUser = null;
 
 // Initialization
